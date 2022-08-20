37ae47cf29c92ede2d23f435555aa6e09c750861
kumartul <kumartul001@gmail.com>
2022-08-20 21:22:21 +0530

    fix: remove the unwanted 'semicolon'

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'next-themes';
 function MyApp({ Component, pageProps }) {
 	return (
 		<ThemeProvider attribute="class">
-			<Component {...pageProps} />;
+			<Component {...pageProps} />
 		</ThemeProvider>
 	);
 }

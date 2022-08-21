30c2d7e45072cb1c412a2d0e1ef8e7b3ddc41699
kumartul <kumartul001@gmail.com>
2022-08-21 15:24:45 +0530

    feat: make the control buttons functional

--- components/ControlButtons/ControlButton.js
+++ components/ControlButtons/ControlButton.js
@@ -1,10 +1,11 @@
 import { IconButton } from '@mui/material';
 
-const ControlButton = ({ Icon, title }) => {
+const ControlButton = ({ Icon, title, onClick }) => {
 	return (
 		<IconButton
 			title={title}
-			className="p-4 rounded-full text-gray-500 bg-gray-200 dark:text-gray-100 dark:bg-[#222]"
+			onClick={onClick}
+			className="p-4 rounded-full !text-gray-500 bg-gray-200 dark:!text-gray-100 dark:bg-[#222]"
 		>
 			<Icon />
 		</IconButton>
--- components/ControlButtons/ControlButtons.js
+++ components/ControlButtons/ControlButtons.js
@@ -4,7 +4,8 @@ import { FiCrop } from 'react-icons/fi';
 import {
 	BiMove,
 	BiZoomIn,
-	BiZoomOut
+	BiZoomOut,
+	BiReset
 } from 'react-icons/bi';
 import {
 	BsArrowLeft,
@@ -21,21 +22,100 @@ import {
 	GiVerticalFlip
 } from 'react-icons/gi';
 
-const ControlButtons = () => {
+const ControlButtons = ({ cropper }) => {
+	const zoomIn = () => cropper.zoom(0.1);
+
+	const zoomOut = () => cropper.zoom(-0.1);
+
+	const moveLeft = () => cropper.move(-10, 0);
+
+	const moveRight = () => cropper.move(10, 0);
+
+	const moveUp = () => cropper.move(0, -10);
+
+	const moveDown = () => cropper.move(0, 10);
+
+	const flipX = () => cropper.getData().scaleX === 1 ? cropper.scaleX(-1) : cropper.scaleX(1);
+
+	const flipY = () => cropper.getData().scaleY === 1 ? cropper.scaleY(-1) : cropper.scaleY(1);
+
+	const rotateLeft = () => cropper.rotate(-90);
+
+	const rotateRight = () => cropper.rotate(90);
+
+	const crop = () => cropper.setDragMode('crop');
+
+	const move = () => cropper.setDragMode('move');
+
+	const reset = () => cropper.reset();
+
 	return (
-		<div className="mt-24 flex items-center justify-center space-x-2">
-			<ControlButton Icon={BiZoomIn} title="Zoom In" />
-			<ControlButton Icon={BiZoomOut} title="Zoom Out" />
-			<ControlButton Icon={BsArrowLeft} title="Move Left" />
-			<ControlButton Icon={BsArrowRight} title="Move Right" />
-			<ControlButton Icon={BsArrowUp} title="Move Up" />
-			<ControlButton Icon={BsArrowDown} title="Move Down" />
-			<ControlButton Icon={GiHorizontalFlip} title="Flip X" />
-			<ControlButton Icon={GiVerticalFlip} title="Flip Y" />
-			<ControlButton Icon={FiCrop} title="Crop" />
-			<ControlButton Icon={BiMove} title="Move" />
-			<ControlButton Icon={GrRotateLeft} title="Rotate Left" />
-			<ControlButton Icon={GrRotateRight} title="Rotate Right" />
+		<div className="mt-24 flex items-center justify-center space-x-4">
+			<ControlButton
+				Icon={BiZoomIn}
+				title="Zoom In"
+				onClick={zoomIn}
+			/>
+			<ControlButton
+				Icon={BiZoomOut}
+				title="Zoom Out"
+				onClick={zoomOut}
+			/>
+			<ControlButton
+				Icon={BsArrowLeft}
+				title="Move Left"
+				onClick={moveLeft}
+			/>
+			<ControlButton
+				Icon={BsArrowRight}
+				title="Move Right"
+				onClick={moveRight}
+			/>
+			<ControlButton
+				Icon={BsArrowUp}
+				title="Move Up"
+				onClick={moveUp}
+			/>
+			<ControlButton
+				Icon={BsArrowDown}
+				title="Move Down"
+				onClick={moveDown}
+			/>
+			<ControlButton
+				Icon={GiHorizontalFlip}
+				title="Flip X"
+				onClick={flipX}
+			/>
+			<ControlButton
+				Icon={GiVerticalFlip}
+				title="Flip Y"
+				onClick={flipY}
+			/>
+			<ControlButton
+				Icon={GrRotateLeft}
+				title="Rotate Left"
+				onClick={rotateLeft}
+			/>
+			<ControlButton
+				Icon={GrRotateRight}
+				title="Rotate Right"
+				onClick={rotateRight}
+			/>
+			<ControlButton
+				Icon={FiCrop}
+				title="Crop"
+				onClick={crop}
+			/>
+			<ControlButton
+				Icon={BiMove}
+				title="Move"
+				onClick={move}
+			/>
+			<ControlButton
+				Icon={BiReset}
+				title="Reset"
+				onClick={reset}
+			/>
 		</div>
 	);
 }

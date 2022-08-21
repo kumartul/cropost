254b9c89b33e3b7eecbda7350308b881c886b22b
kumartul <kumartul001@gmail.com>
2022-08-21 15:54:46 +0530

    rename: 'dimensions.js' to 'resolutions.js'

---
+++ utils/resolution.js
@@ -0,0 +1,263 @@
+const resolutions = [
+	{
+		GitHub: [
+			{
+				name: "Profile Avatar",
+				description: "Your Profile Avatar picture",
+				width: 200,
+				height: 200
+			},
+			{
+				name: "Repository Cover",
+				description: "The Cover image of the Repository",
+				width: 1280,
+				height: 640,
+			}
+		]
+	},
+	{
+		Twitter: [
+			{
+				name: "Timeline Photo",
+				description: "An image that you would post in your Timeline",
+				width: 1200,
+				height: 675,
+			},
+			{
+				name: "Profile Cover",
+				description: "An image that will be shown as you Profile Cover",
+				width: 1500,
+				height: 500,
+			},
+			{
+				name: "Profile Avatar",
+				description: "Your Profile Avatar picture",
+				width: 400,
+				height: 400,
+			},
+		],
+	},
+	{
+		Instagram: [
+			{
+				name: "Landscape Photo",
+				description: "An image that you would post in your Timeline",
+				width: 1080,
+				height: 680,
+			},
+			{
+				name: "Portrait Photo",
+				description: "An image that will be shown as you Profile Cover",
+				width: 1080,
+				height: 1080,
+			},
+			{
+				name: "Profile Avatar",
+				description: "Your Profile Avatar picture",
+				width: 320,
+				height: 320,
+			},
+		],
+	},
+	{
+		Facebook: [
+			{
+				name: "Timeline Photo",
+				description: "An image that you would post in your Timeline",
+				width: 1200,
+				height: 630,
+			},
+			{
+				name: "Profile Cover",
+				description: "An image that will be shown as you Profile Cover",
+				width: 820,
+				height: 360,
+			},
+			{
+				name: "Profile Avatar",
+				description: "Your Profile Avatar picture",
+				width: 200,
+				height: 200,
+			},
+		],
+	},
+	{
+		YouTube: [
+			{
+				name: "Video Thumbnail",
+				description:
+					"A preview image that will be used as a Cover for the Video",
+				width: 1280,
+				height: 720,
+			},
+			{
+				name: "Channel Cover",
+				description: "An image that will be shown as you Profile Cover",
+				width: 2560,
+				height: 1440,
+			},
+			{
+				name: "Profile Avatar",
+				description: "Your Profile Avatar picture",
+				width: 800,
+				height: 800,
+			},
+		],
+	},
+	{
+		LinkedIn: [
+			{
+				name: "Timeline Photo",
+				description: "An image that you would post in your Timeline",
+				width: 1200,
+				height: 627,
+			},
+			{
+				name: "Profile Cover",
+				description: "An image that will be shown as you Profile Cover",
+				width: 1128,
+				height: 191,
+			},
+			{
+				name: "Profile Avatar",
+				description: "Your Profile Avatar picture",
+				width: 268,
+				height: 268,
+			},
+		],
+	},
+	{
+		Hashnode: [
+			{
+				name: "Article Cover",
+				description: "The Cover image of the Article",
+				width: 1600,
+				height: 840,
+			},
+			{
+				name: "Blog Cover",
+				description: "The Cover image of Blog page",
+				width: 1600,
+				height: 480,
+			},
+			{
+				name: "Profile Avatar",
+				description: "Your Profile Avatar picture",
+				width: 400,
+				height: 400,
+			},
+			{
+				name: "Publication Logo",
+				description: "Replaces the Blog title on Homepage and Post pages",
+				width: 500,
+				height: 125,
+			},
+			{
+				name: "Publication Thumbnail",
+				description:
+					"Shown as the Blog's Logo on mobile screens and used as your Blog's Favicon",
+				width: 500,
+				height: 500,
+			},
+			{
+				name: "Social Media Sharing",
+				description: "Used when your Blog is shared on Twitter, Facebook, etc",
+				width: 800,
+				height: 420,
+			},
+		],
+	},
+	{
+		DEV: [
+			{
+				name: "Article Cover",
+				description: "The Cover image of the Article",
+				width: 1000,
+				height: 420,
+			},
+			{
+				name: "Profile Avatar",
+				description: "Your Profile Avatar picture",
+				width: 320,
+				height: 320,
+			},
+		],
+	},
+	{
+		Medium: [
+			{
+				name: "Article Cover",
+				description: "The Cover image of the Article",
+				width: 1400,
+				height: 788,
+			},
+
+			{
+				name: "Blog Cover",
+				description: "The Cover image of the Article",
+				width: 1500,
+				height: 750,
+			},
+
+			{
+				name: "Profile Avatar",
+				description: "Your Profile Avatar picture",
+				width: 1000,
+				height: 1000,
+			},
+			{
+				name: "Full-width Image",
+				description: "Maxed out image in your Articles",
+				width: 2040,
+				height: 1148,
+			},
+		],
+	},
+	{
+		Hackernoon: [
+			{
+				name: "Article Cover",
+				description: "The Cover image of the Article",
+				width: 1920,
+				height: 1080,
+			},
+			{
+				name: "Profile Avatar",
+				description: "Your Profile Avatar picture",
+				width: 460,
+				height: 460,
+			},
+		],
+	},
+	{
+		Pinterest: [
+			{
+				name: "Square Pin",
+				description: "A square Pin size",
+				width: 1000,
+				height: 1000,
+			},
+			{
+				name: "Vertical Pin",
+				description: "A standard vertical-oriented Pin",
+				width: 1000,
+				height: 1500,
+			},
+			{
+				name: "Giraffe Pin",
+				description: "An extended vertical-oriented Pin",
+				width: 1000,
+				height: 2100,
+			},
+			{
+				name: "Profile Avatar",
+				description: "Your Profile Avatar picture",
+				width: 280,
+				height: 280,
+			},
+		],
+	},
+
+];
+
+export default resolutions;
